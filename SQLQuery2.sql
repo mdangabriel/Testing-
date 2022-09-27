12652028e635a579c51d552b0f37da40fd0b8882
mdangabriel <114519600+mdangabriel@users.noreply.github.com>
2022-09-27 11:33:18 -0700

    Create SQLQuery2.sql
    first commit

---
+++ SQLQuery2.sql
@@ -0,0 +1,20 @@
+SELECT COUNT(IDTranzactie) as Tranzactii
+FROM Tranzactii
+WHERE IDClient = 1
+
+SELECT COUNT(IDTranzactie) as Tranzactii
+FROM Tranzactii
+WHERE IDClient = 1 AND IDProdus = 123
+
+SELECT COUNT(IDTranzactie) as Tranzactii
+FROM Tranzactii
+WHERE IDClient = 1 AND IDProdus = 124
+
+SELECT COUNT(IDTranzactie) as "Produse Cumparate"
+FROM Tranzactii
+WHERE IDProdus = 124
+
+SELECT COUNT(IDTranzactie) as "Produse Cumparate"
+FROM Tranzactii
+WHERE IDProdus = 123
+
